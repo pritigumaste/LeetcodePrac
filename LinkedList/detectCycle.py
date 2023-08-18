57fb9d1c6f895a13ba1367378d9c0db6d48c0240
Priti Gumaste <56169162+pritigumaste@users.noreply.github.com>
2023-08-18 17:06:53 -0400

    Create detectCycle.py

---
+++ LinkedList/detectCycle.py
@@ -0,0 +1,36 @@
+#https://leetcode.com/problems/linked-list-cycle-ii/editorial/
+#LINKEDLIST CYCLE 2 
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+#Using the floyd's hare and tortoise algorithm
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                break
+        
+        if not fast or not fast.next:
+            return None 
+        
+        #START OF SECOND ROUND bcz fast and slow ran into each other in 1st one
+        #resetting the fast pointer
+        fast = head
+        #second round we just increment the fast pointer by 1 onli
+        while fast != slow:
+            fast = fast.next
+            slow = slow.next
+
+        return fast
