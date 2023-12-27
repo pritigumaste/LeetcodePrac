481389fc378aa135b7bd8af50da44a10eb8c5d74
Priti Gumaste <56169162+pritigumaste@users.noreply.github.com>
2023-12-26 19:55:42 -0500

    Update ProductArrexceptSelf.py

--- LeetCodePatterns/ProductArrexceptSelf.py
+++ LeetCodePatterns/ProductArrexceptSelf.py
@@ -1,7 +1,9 @@
 #https://leetcode.com/problems/product-of-array-except-self/description/
 #time - 0(n)
 #space = 0(n) - because we are using extra space to store left and right running products 
-
+#this intution came, if we are to take any random number and are asked for running product 
+#then we can say we will need product of numbers at left to product of numbers at right. 
+#hence the running product of left and right side of numbers should get us the answer. 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         length = len(nums)
