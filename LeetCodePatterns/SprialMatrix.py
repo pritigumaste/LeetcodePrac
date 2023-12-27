9ef5fc43cee3dc6f81e2793cd77612edd4a21fa6
Priti Gumaste <56169162+pritigumaste@users.noreply.github.com>
2023-12-27 13:54:56 -0500

    Update SprialMatrix.py
    added one more solution

--- LeetCodePatterns/SprialMatrix.py
+++ LeetCodePatterns/SprialMatrix.py
@@ -31,4 +31,61 @@ class Solution:
             clast -=1
         return result
 
+#ONE MORE SOLUTION BELOW 
+-----------------------------------------------------------------------------------------
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        m = len(matrix)
+        n = len(matrix[0])
+        result = []
+
+        left =0 
+        right = n-1
+        top = 0
+        bottom = m-1
+        #to ensure boundary conditions are met, we need to check that these don't 
+        #cross each other
+        while(top <= bottom and left <= right):
+
+            #traversing top row
+            for i in range(left, right+1):
+                result.append(matrix[top][i])
+            #once row 0 is iterated we are never coming back again 
+            #so need to increment the row number to next one
+            top +=1
+
+            #traversing right col
+            #no need to check base condition here because we aren't modifying 
+            #left and right before proceeding 
+            #if (top <= bottom and left <= right):
+            for j in range(top, bottom+1):
+                result.append(matrix[j][right])
+            #decrementing the column number 
+            right -=1
+
+            #print(matrix[bottom][right])
+            #traversing bottom row 
+            if (top <= bottom):
+                for k in range(right , left-1, -1):
+                #print(matrix[bottom][k])
+                    result.append(matrix[bottom][k])
+            #incrementing the row number
             
+            bottom -=1 
+
+            
+            if (left <= right):
+            #traversing left col 
+                for l in range(bottom, top-1, -1):
+                    result.append(matrix[l][left])
+            left+=1
+
+
+        
+        return result 
+
+
+
+
+        
