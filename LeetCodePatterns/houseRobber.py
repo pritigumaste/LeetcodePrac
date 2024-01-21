07e87f3d8317259ea0935399084571a14e2b2b77
Priti Gumaste <56169162+pritigumaste@users.noreply.github.com>
2024-01-20 21:42:18 -0500

    Create houseRobber.py

---
+++ LeetCodePatterns/houseRobber.py
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+
+
+-----------------------------------------------------------------------
+#Time is EXPONENTIAL 
+#this will give TLE 
+    #NEED TO USE MEMOIZATION 
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if nums is None:
+            return 0
+
+        return self.check(0, nums)
+
+    def check(self, i , nums):
+        sumMax =0 
+        if i >= len(nums):
+            return 0
+
+        sumMax = max(self.check(i+2, nums) + nums[i], self.check(i+1, nums))
+        #print(nums[i+2], "nums[i+2")
+        # print(nums[i], "nums[i]")
+        # print(sumMax)
+        return sumMax
+        
