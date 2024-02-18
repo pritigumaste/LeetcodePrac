cf5629ce5e4fc68d6c6023da003e2d71de6f1783
Priti Gumaste <56169162+pritigumaste@users.noreply.github.com>
2024-02-18 15:31:56 -0500

    Create Subsets_recursion.py

---
+++ BackTracking/Subsets_recursion.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+
+        def helper(nums, i , arr):
+            #base condition
+            if (i==len(nums)):
+                result.append(arr)
+                return
+            #if not choose
+            helper(nums, i+1, arr.copy())
+            #if choose 
+            arr.append(nums[i])
+            helper(nums, i+1 , arr.copy())
+
+        
+        if len(nums) ==0:
+            return 
+        result=[]
+        helper(nums, 0, [])
+        return result
+        
