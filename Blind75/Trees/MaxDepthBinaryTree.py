1bff70e02d22ca704d4bb11a9e7af7b8f3425487
Priti Gumaste <56169162+pritigumaste@users.noreply.github.com>
2024-04-06 15:10:59 -0400

    Create MaxDepthBinaryTree.py

---
+++ Blind75/Trees/MaxDepthBinaryTree.py
@@ -0,0 +1,41 @@
+#time: O(n)
+#space: O(n)
+
+#https://leetcode.com/problems/maximum-depth-of-binary-tree/editorial/
+
+
+#ITERATIVE BFS ----------------------
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0 
+        levels = 0
+        queue = collections.deque([root])
+        while queue:
+           # print(queue)
+            for i in range(len(queue)):
+                curr = queue.popleft()
+        
+                if curr.left:
+                    queue.append(curr.left)
+
+                if curr.right:
+                    queue.append(curr.right)
+            levels+=1
+        return levels
+
+
+#time: O(n)
+#space: O(h)
+
+#RECURSIVE DFS
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0 
+
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+
+        return max(left,right) +1 
