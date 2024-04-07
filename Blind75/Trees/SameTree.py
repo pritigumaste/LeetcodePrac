fe102e4239a310299cad5b307f43d3a248036fe2
Priti Gumaste <56169162+pritigumaste@users.noreply.github.com>
2024-04-07 14:54:24 -0400

    Update SameTree.py
    added iterative way

--- Blind75/Trees/SameTree.py
+++ Blind75/Trees/SameTree.py
@@ -18,3 +18,34 @@ class Solution:
 
         return self.isSameTree(p.right, q.right)and self.isSameTree(p.left, q.left)
         
+#Iterative
+#time: O(n) 
+#space: o(n) - queue
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+
+        def isSame(p,q):
+            if p == None and q == None:
+                return True
+
+            if p == None or q == None:
+                return False 
+        
+            if p.val != q.val:
+                return False
+            return True
+        
+        queue = collections.deque([(p,q)])
+        while queue:
+            p,q = queue.popleft()
+
+            if not isSame(p,q):
+                return False
+            
+            if p:
+                queue.append((p.left, q.left))
+                queue.append((p.right, q.right))
+
+
+        return True
